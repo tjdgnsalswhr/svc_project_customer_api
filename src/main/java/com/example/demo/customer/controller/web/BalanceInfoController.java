791dcc836ab382b2e9550bdc5006622487904099
holidenty <gldiwkd@ajou.ac.kr>
2020-08-11 18:03:27 +0900

    ....

--- src/main/java/com/example/demo/customer/controller/web/BalanceInfoController.java
+++ src/main/java/com/example/demo/customer/controller/web/BalanceInfoController.java
@@ -3,6 +3,7 @@ package com.example.demo.customer.controller.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.example.demo.customer.core.service.BalanceInfoService;
 
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin("*")
 @RestController
 public class BalanceInfoController {
 	
--- src/main/java/com/example/demo/customer/controller/web/BillingController.java
+++ src/main/java/com/example/demo/customer/controller/web/BillingController.java
@@ -4,6 +4,7 @@ package com.example.demo.customer.controller.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.example.demo.customer.core.service.BillingService;
 
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin("*")
 @RestController
 public class BillingController {
 	
